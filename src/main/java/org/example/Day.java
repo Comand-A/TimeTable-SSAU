d5d1b248dc14a2bc834d6e7bfa34f52e76f69d35
Egor <65020062+korgi12@users.noreply.github.com>
2022-10-06 22:50:55 +0400

    Egor_Arsen_Version.6
    WE DID THIS!!!!!

--- src/main/java/org/example/Day.java
+++ src/main/java/org/example/Day.java
@@ -1,21 +1,44 @@
 package org.example;
 
 public class Day {
-    private String data, para1, para2, para3, para4, para5;
+    public Day(String data, String paraFirst, String placeFirst, String teacherFirst,
+               String paraSecond, String placeSecond, String teacherSecond,
+               String paraThird, String placeThird, String teacherThird,
+               String paraFourth, String placeFourth, String teacherFourth,
+               String paraFifth, String placeFifth, String teacherFifth) {
+        this.data = data;
+        this.paraFirst = paraFirst;
+        this.placeFirst = placeFirst;
+        this.teacherFirst = teacherFirst;
+        this.paraSecond = paraSecond;
+        this.placeSecond = placeSecond;
+        this.teacherSecond = teacherSecond;
+        this.paraThird = paraThird;
+        this.placeThird = placeThird;
+        this.teacherThird = teacherThird;
+        this.paraFourth = paraFourth;
+        this.placeFourth = placeFourth;
+        this.teacherFourth = teacherFourth;
+        this.paraFifth = paraFifth;
+        this.placeFifth = placeFifth;
+        this.teacherFifth = teacherFifth;
+    }
+
+    private String data, paraFirst,placeFirst,teacherFirst,
+                         paraSecond,placeSecond,teacherSecond,
+                         paraThird,placeThird,teacherThird,
+                         paraFourth,placeFourth,teacherFourth,
+                         paraFifth,placeFifth,teacherFifth;
 
     @Override
     public String toString() {
-        return String.format("Дата:%s\nПара1:%s\nПара2:%s\nПара3:%s\nПара4:%s\nПара5:%s\n", this.data,this.para1, this.para2, this.para3, this.para4, this.para5);
+        return String.format("Дата:%s\nПара1:%s\nМесто:%s\nУчитель:%s\n\nПара2:%s\nМесто:%s\nУчитель:%s\n\nПара3:%s" +
+                        "\nМесто:%s\nУчитель:%s\n\nПара4:%s\nМесто:%s\nУчитель:%s\n\nПара5:%s\nМесто:%s\nУчитель:%s\n\n",
+                this.data,this.paraFirst,this.placeFirst ,this.teacherFirst ,
+                this.paraSecond ,this.placeSecond ,this.teacherSecond,
+                this.paraThird,this.placeThird ,this.teacherThird,
+                this.paraFourth ,this.placeFourth ,this.teacherFourth,
+                this.paraFifth ,this.placeFifth ,this.teacherFifth);
+    }
     }
 
-    public Day(String data, String para1, String para2, String para3, String para4, String para5) {
-        this.data = data;
-        this.para1 = para1;
-        this.para2 = para2;
-        this.para3 = para3;
-        this.para4 = para4;
-        this.para5 = para5;
-    }
-
-
-}
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class Parser {
     public Document page;
     public Element table;
-    public ArrayList<String> week, schedulePlace, dates, anotherDays, monday;
+    public ArrayList<String> week, schedulePlace, dates, anotherDays, monday, teachers;
     public List<Day> timeTable;
 
 
@@ -26,42 +26,59 @@ public class Parser {
     }
 
     public Document getPage() throws IOException {
-        String url = "https://ssau.ru/rasp?groupId=755922237&selectedWeek=2&selectedWeekday=1";
+        String url = "https://ssau.ru/rasp?groupId=755922237&selectedWeek=6&selectedWeekday=1";
         page = Jsoup.parse(new URL(url), 5000);
-        return page;
+        return this.page;
     }
 
     private ArrayList<String> getSchedulePlace() {
-        String cssQuery = "div[class=caption-text schedule__place]";
-        Elements SchedulePlaces = table.select(cssQuery);
+        String[] cssQueryArray = {"div[class=schedule__item schedule__item_show]", "div[class=caption-text schedule__place]", "div[class=schedule__item]"};
         schedulePlace = new ArrayList<>();
-        for (Element Place : SchedulePlaces) {
-            schedulePlace.add(Place.select(cssQuery).text());
+        Elements schedulePlaceMonday = table.select(cssQueryArray[0]);
+        Elements schedulePlaceAnotherDays = table.select(cssQueryArray[2]);
+        for (Element place : schedulePlaceMonday) {
+            schedulePlace.add(place.select(cssQueryArray[1]).text());
         }
-        return schedulePlace;
+        for (Element place : schedulePlaceAnotherDays) {
+            schedulePlace.add(place.select(cssQueryArray[1]).text());
+        }
+        return this.schedulePlace;
+    }
+
+    private ArrayList<String> getTeacher() {
+        String[] cssQueryArray = {"div[class=schedule__item schedule__item_show]", "div[class=schedule__teacher]", "div[class=schedule__item]"};
+        Elements teacherMonday = table.select(cssQueryArray[0]);
+        Elements teacherAnotherDays = table.select(cssQueryArray[2]);
+        teachers = new ArrayList<>();
+        for (Element teacher : teacherMonday) {
+            teachers.add(teacher.select(cssQueryArray[1]).text());
+        }
+        for (Element teacher : teacherAnotherDays) {
+            teachers.add(teacher.select(cssQueryArray[1]).text());
+        }
+        return this.teachers;
     }
 
     public ArrayList<String> getMonday() {
         String cssQuery = "div[class=schedule__item schedule__item_show]";
-        String[] cssQueryArray = {"div[class=body-text schedule__discipline lesson-color lesson-color-type-","1]","2]","3]","4]"};
+        String[] cssQueryArray = {"div[class=body-text schedule__discipline lesson-color lesson-color-type-", "1]", "2]", "3]", "4]"};
         Elements Mondays = table.select(cssQuery);
         week = new ArrayList<>();
-        int i=0;
+        int i = 0;
         for (Element Monday : Mondays) {
             if (!Monday.select(cssQuery).text().equals("")) {
-                for (int j=1;j<5;j++){
-                    week.add(Monday.select(cssQueryArray[0]+cssQueryArray[j]).text());
-                    if (week.get(i).equals("")) {week.remove(i);}
+                for (int j = 1; j <= 4; j++) {
+                    if (!Monday.select(cssQueryArray[0] + cssQueryArray[j]).text().equals("")) {
+                        week.add(Monday.select(cssQueryArray[0] + cssQueryArray[j]).text());
+                    }
                 }
+
+            } else {
+                week.add("Нет пары");
             }
             i++;
         }
-        for (int n=0;n<5;n++){
-            if (week.get(n).equals("")){
-                week.set(n,"Нет пары");
-            }
-        }
-        return week;
+        return this.week;
     }
 
     private ArrayList<String> getDate() {
@@ -76,47 +93,58 @@ public class Parser {
 
     public ArrayList<String> getAnotherDays() {
         String cssQuery = "div[class=schedule__item]";
-        String[] cssQueryArray = {"div[class=body-text schedule__discipline lesson-color lesson-color-type-","1]","2]","3]","4]"};
+        String[] cssQueryArray = {"div[class=body-text schedule__discipline lesson-color lesson-color-type-", "1]", "2]", "3]", "4]"};
         Elements allDay = table.select(cssQuery);
         anotherDays = new ArrayList<>();
-        int i=0;
+        int i = 0;
         for (Element day : allDay) {
             if (!day.select(cssQuery).text().equals("")) {
-                for (int j=1;j<5;j++){
-                    anotherDays.add(day.select(cssQueryArray[0]+cssQueryArray[j]).text());
-                    if (anotherDays.get(i).equals("")) {anotherDays.remove(i);}
+                for (int j = 1; j < 5; j++) {
+                    if (!day.select(cssQueryArray[0] + cssQueryArray[j]).text().equals("")) {
+                        anotherDays.add(day.select(cssQueryArray[0] + cssQueryArray[j]).text());
+                    }
                 }
+            } else {
+                anotherDays.add("Нет пары");
             }
             i++;
         }
-        for (int n=0;n<anotherDays.size();n++){
-            if (anotherDays.get(n).equals("")){
-                anotherDays.set(n,"Нет пары");
-            }
-        }
-        return anotherDays;
+        return this.anotherDays;
     }
 
     public List<Day> getTimeTable() {
         monday = this.getMonday();
         dates = this.getDate();
         anotherDays = this.getAnotherDays();
-        timeTable = new ArrayList<>(Collections.singletonList(new Day(dates.get(0), monday.get(0), monday.get(1), monday.get(2), monday.get(3), monday.get(4))));
+        teachers = this.getTeacher();
+        schedulePlace = this.getSchedulePlace();
+        timeTable = new ArrayList<>(Collections.singletonList(new Day(dates.get(0),
+                monday.get(0), schedulePlace.get(0), teachers.get(0),
+                monday.get(1), schedulePlace.get(1), teachers.get(1),
+                monday.get(2), schedulePlace.get(2), teachers.get(2),
+                monday.get(3), schedulePlace.get(3), teachers.get(3),
+                monday.get(4), schedulePlace.get(4), teachers.get(4))));
         dates.remove(0);
         anotherDays.addAll(0, dates);
         for (int i = 0; i < 5; i++) {
-            timeTable.add((new Day(anotherDays.get(i), anotherDays.get(i + 5), anotherDays.get(i + 10), anotherDays.get(i + 15), anotherDays.get(i + 20), anotherDays.get(i + 25))));
+            timeTable.add((new Day(anotherDays.get(i),
+                    anotherDays.get(i + 5), schedulePlace.get(i+5), teachers.get(i+5),
+                    anotherDays.get(i + 10), schedulePlace.get(i + 10), teachers.get(i + 10),
+                    anotherDays.get(i + 15), schedulePlace.get(i + 15), teachers.get(i + 15),
+                    anotherDays.get(i + 20), schedulePlace.get(i + 20), teachers.get(i + 20),
+                    anotherDays.get(i + 25), schedulePlace.get(i + 25), teachers.get(i + 25))));
         }
-        return timeTable;
+        return this.timeTable;
     }
 
     public void Print() {
         Parser parser = new Parser();
-        ArrayList<String> schedulePlace = parser.getSchedulePlace();
+//        schedulePlace = getSchedulePlace();
+//        teachers=getTeacher();
         timeTable = parser.getTimeTable();
         for (Day s : timeTable) {
             System.out.println(s);
         }
-        System.out.println(schedulePlace);
+        //System.out.println(schedulePlace+"\n"+teachers);
     }
 }

600ea14c6c06d8a7976effff19e2041977a76493
zamulinarsenii <114849263+zamulinarsenii@users.noreply.github.com>
2022-10-11 17:37:21 +0400

    Arsen.9.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 
 import org.example.Parser.Parser;
+import org.example.Parser.RealDate;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         Parser getData = new Parser();
         getData.Print();
     }
--- src/main/java/org/example/Parser/RealDate.java
+++ src/main/java/org/example/Parser/RealDate.java
@@ -1,21 +1,28 @@
 package org.example.Parser;
 
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class RealDate {
     private Document page;
     private String realDate = page.select("span[class=top-bar__text]").text();
-    private Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
     public RealDate(Document page) {
         this.page = page;
     }
-
-    private String getRealDate() {
-        Matcher matcher = pattern.matcher(realDate);
-        return matcher.group();
+    public long getNumberOfWeek() {
+        long millisecondsInOneWeek = 604800000;
+        Date begin = new Date(1661630400001L);
+        Date realDateInDate;
+        try {
+            realDateInDate = new SimpleDateFormat("dd.MM.yyyy").parse(realDate);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        long numberOfWeek = (((realDateInDate.getTime() - begin.getTime()) / millisecondsInOneWeek) + 1);
+        return numberOfWeek;
     }
 }

ee4b44b038a5049328af9b32488310dac951f393
Egor <65020062+korgi12@users.noreply.github.com>
2022-10-13 13:19:11 +0400

    Egor.9.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,9 @@ import org.example.Parser.RealDate;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Parser getData = new Parser();
-        getData.Print();
+    public static void main(String[] args){
+
+//        Parser getData = new Parser();
+//        getData.Print();
     }
 }
---
+++ src/main/java/org/example/Telegram/TimeTableBot.java
@@ -0,0 +1,17 @@
+package org.example.Telegram;
+
+import org.example.Telegram.service.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TimeTableBot {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
